360b460214895311105bd5c7f187c1ce6015b605
bagelbunny <58603034+bagelbunny@users.noreply.github.com>
2021-02-20 20:37:14 -0500

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const Title = document.querySelector(".logo-heading");
+Title.addEventListener("click", () => {
+  Title.style.color = "green";
+});
+const nav = document.querySelectorAll(".nav-link");
+
+nav[0].addEventlistener("click", (e) => {
+  nav[0].style.color = "red";
+  console.log("This is my event listener ");
+});
